6f7a9af0add6c32aa7dbe8d1b9637db04f49a9bf
PelleRemus <remusnicolaepelle@gmail.com>
2022-11-08 21:19:47 +0200

    Laborator 08.11.2022 - created a People application with search and pagination on the People table

---
+++ 3.4.WebApplicationMVC/3.4.WebApplicationMVC/Controllers/PeopleController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _3._4.WebApplicationMVC.Data;
+using _3._4.WebApplicationMVC.Models;
+
+namespace _3._4.WebApplicationMVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PeopleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: People
+        public async Task<IActionResult> Index(string Search, int Page)
+        {
+            var people = await _context.People.ToListAsync();
+            int pageSize = 2;
+
+            if (!string.IsNullOrWhiteSpace(Search))
+            {
+                people = people.Where(person => SearchInteligent(person, Search))
+                    .ToList();
+            }
+            people = people
+                .Skip((Page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return View(people);
+        }
+
+        // GET: People/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: People/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,NickName,Country,Ocupation,Age")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: People/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,NickName,Country,Ocupation,Age")] Person person)
+        {
+            if (id != person.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: People/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.People.Any(e => e.Id == id);
+        }
+
+        private bool SearchInteligent(Person person, string input)
+        {
+            // avem nevoie de toate numele separate
+            char[] separatori = new char[] { ' ', '-' };
+            string numePrenume = $"{person.FirstName} {person.LastName}";
+            string[] toateNumele = numePrenume.Split(separatori);
+
+            // si inputul trebuie separat
+            string[] inputs = input.Split(separatori);
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                // presupunem ca inputul curent nu apare in nume
+                bool ok = false;
+                // si verificam daca apare in vreun nume
+                for (int j = 0; j < toateNumele.Length; j++)
+                    if (toateNumele[j].ToLower().Contains(inputs[i].ToLower()))
+                        ok = true;
+
+                if (!ok)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
