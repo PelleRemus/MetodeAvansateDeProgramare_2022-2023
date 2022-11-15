824a6de87121c02f8e93cc1afd51fe4b0b8cd749
PelleRemus <remusnicolaepelle@gmail.com>
2022-11-15 19:52:45 +0200

    "in-memory" database for the person Model

---
+++ 3.5.WebAppREST_API/3.5.WebAppREST_API/Controllers/PeopleController.cs
@@ -0,0 +1,66 @@
+﻿using _3._5.WebAppREST_API.Models;
+using _3._5.WebAppREST_API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _3._5.WebAppREST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleRepository _peopleRepository; 
+
+        public PeopleController()
+        {
+            _peopleRepository = new PeopleRepository();
+        }
+
+        // GET: api/People
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _peopleRepository.people;
+        }
+
+        // GET api/People/5
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return _peopleRepository.people.FirstOrDefault(person => person.Id == id);
+        }
+
+        // POST api/People
+        [HttpPost]
+        public void Post([FromBody] Person body)
+        {
+            _peopleRepository.people.Add(body);
+        }
+
+        // PUT api/People/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Person body)
+        {
+            Person dbPerson = _peopleRepository.people.FirstOrDefault(person => person.Id == id);
+
+            dbPerson.FirstName = body.FirstName;
+            dbPerson.LastName = body.LastName;
+            dbPerson.NickName = body.NickName;
+            dbPerson.Ocupation = body.Ocupation;
+            dbPerson.Country = body.Country;
+            dbPerson.Age = body.Age;
+        }
+
+        // DELETE api/People/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _peopleRepository.people.RemoveAll(person => person.Id == id);
+        }
+    }
+}
---
+++ 3.5.WebAppREST_API/3.5.WebAppREST_API/Repositories/PeopleRepository.cs
@@ -0,0 +1,19 @@
+﻿using _3._5.WebAppREST_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3._5.WebAppREST_API.Repositories
+{
+    public class PeopleRepository
+    {
+        public List<Person> people = new List<Person>()
+        {
+            new Person() { Id = 1, FirstName = "Remus", LastName = "Pelle", Age = 23,
+                Country = "Romania", NickName = "Nicholas", Ocupation = "Prof" },
+            new Person() { Id = 2, FirstName = "Marcela", LastName = "Doe", Age = 54,
+                Country = "UK", NickName = "Marci", Ocupation = "None" },
+        };
+    }
+}
